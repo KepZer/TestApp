baa5a08dbaddea3624714a081aeb79570ff5d5dc
KepZer <86748409+KepZer@users.noreply.github.com>
2021-07-21 21:11:54 +0300

    Добавьте файлы проекта.

---
+++ TestApp/routes/index.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var tMod = require('bindings')('TestModule');
+
+
+//title: testModule.ContainsUser("Гость") 
+router.get('/', function (req, res) {
+    res.render('index', {});
+});
+
+router.get('/check', function (req, res) {
+    var name = req.param("userName");
+    if (tMod.ContainsUser(name)) {
+        console.log("yes");
+    } else {
+        console.log("no");
+    }
+
+
+    res.render('index', {});
+});
+
+module.exports = router;
